36e32c8a9cbef39795628b9b311ffac7c8df5064
jnc0x24dd099bb870 <35341430+jnc0x24dd099bb870@users.noreply.github.com>
2019-10-31 22:18:23 +0200

    Create heh.py

---
+++ pipel1line/ftp/heh.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3 
+
+# plain ol' server_forever
+# and serve weather_data.csv
+
+import logging
+
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+from pyftpdlib.authorizers import DummyAuthorizer
+
+authorizer = DummyAuthorizer()
+authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
+handler = FTPHandler
+handler.authorizer = authorizer
+
+logging.basicConfig(filename='/var/log/pyftpd.log', level=logging.INFO)
+
+server = FTPServer(('', 2121), handler)
+server.serve_forever()
