465af0d93069d6fea7178a608ea6a194d860de7f
jnc0x24dd099bb870 <35341430+jnc0x24dd099bb870@users.noreply.github.com>
2019-11-01 00:45:29 +0200

    Create script_cred.py

---
+++ pipel1line/process/script_cred.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+def cred_script():
+
+    str="""#!/usr/bin/python3
+import os
+import sys
+
+
+class User:
+    name='root'
+u1=User()
+#print(u1.name)
+
+class Passwd:
+    secret='abc123'
+p1=Passwd()
+
+class Host:
+    hostname="172.17.0.2"
+h1=Host()
+
+class Dba:
+    database="huehue"
+d1=Dba()
+
+"""
+
+    m_m = open("/home/wild_project/py/ha/cr0n/meh/ccc.py", "w")
+    m_m.write(str)
+    m_m.close()
