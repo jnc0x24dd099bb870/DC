61544eb292729e7987311efd1ea164140fc9e04c
jnc0x24dd099bb870 <35341430+jnc0x24dd099bb870@users.noreply.github.com>
2019-10-31 22:14:28 +0200

    Create ccc.py

---
+++ pipel1line/db/ccc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+
+class User:
+    name='root'
+u1=User()
+#print(u1.name)
+
+class Passwd:
+    secret='abc123'
+p1=Passwd()
+
+class Host:
+    hostname="172.17.0.2"
+h1=Host()
+
+class Dba:
+    database="huehue"
+d1=Dba()
