b8394a48b30feec1582c0065694f3f9b1f38cc46
jnc0x24dd099bb870 <35341430+jnc0x24dd099bb870@users.noreply.github.com>
2019-10-31 22:03:45 +0200

    Create main_prog.py

---
+++ kube/main_prog.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import updating_system
+import installing_services
+import source_update
+import install_k8s
+
+updating_system.Updates().upd()
+updating_system.Updates().install()
+
+installing_services.Services().docker_installing()
+source_update.install()
+
+install_k8s.Install().pip_client()
+
+#still in progress, though... need to set-up others, like network
