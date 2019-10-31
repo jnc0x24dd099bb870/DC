ffb28c9912cfb41d29d223a9b54cdb0a7395d596
jnc0x24dd099bb870 <35341430+jnc0x24dd099bb870@users.noreply.github.com>
2019-11-01 00:13:58 +0200

    Create script_ftp.py

---
+++ pipel1line/process/script_ftp.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import docker
+import os, subprocess
+import sys, six
+
+from script_dba import dba_script
+
+def ftp_script():
+
+    str = """#!/usr/bin/python3
+import ftplib
+from ftplib import FTP
+from pathlib import Path
+import os
+
+ftp = FTP()
+ftp.connect('172.17.0.1', 2121)
+ftp.login('user', '12345')
+ftp.pwd()
+
+ftp.cwd('/')
+
+def getFile(ftp, filename):
+    try:
+        ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
+    except: 
+        print("perror")
+
+getFile(ftp, 'weather_data.csv')
+
+ftp.quit()
+
+os.system("cp /root/weather_data.csv /home/")
+"""
+
+    f_f = open("/home/wild_project/py/ha/cr0n/meh/ftpy.py", "w")
+    f_f.write(str)
+    f_f.close()
+
