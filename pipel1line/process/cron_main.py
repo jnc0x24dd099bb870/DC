1971fc1a47c53a724eb9a0e2fe9e7d1b7e66603f
jnc0x24dd099bb870 <35341430+jnc0x24dd099bb870@users.noreply.github.com>
2019-11-01 00:24:01 +0200

    Create cron_main.py

---
+++ pipel1line/process/cron_main.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+import docker
+import os, subprocess
+import sys, six
+
+from script_dba import dba_script
+from script_ftp import ftp_script
+from script_cron import cron_script
+
+
+def BuildImage():
+    dockerfile = '''
+    FROM ubuntu:16.04
+    RUN touch /var/log/cron.log
+    RUN apt-get update
+    RUN apt-get install curl -y
+    RUN apt-get install build-essential -y
+    RUN apt-get install python3 -y
+    RUN apt-get install software-properties-common -y
+    RUN apt-get install python3-setuptools -y
+    RUN apt-get install python3-pip -y
+    RUN apt-get update
+    RUN pip3 install pymysql
+    RUN pip3 install mysql-connector-python
+    RUN pip3 install sqlalchemy
+    RUN pip3 install pandas
+    RUN apt-get -y install cron
+    CMD cron && tail -f /var/log/cron.log 
+    '''
+
+    f = six.BytesIO(dockerfile.encode('utf-8'))
+
+    cli = docker.APIClient(base_url='unix://var/run/docker.sock')
+
+    response = [line for line in cli.build(fileobj=f, rm=True, tag='croney:latest')]
+
+    print(response)
+    print("      ")
+    print("The Cron image is now built...")
+
+def CreateContainerCron():
+    print("starting to build container")
+    client = docker.APIClient(base_url='unix://var/run/docker.sock')
+    
+    container_id = client.create_container('croney', name='cronak', ports=[2323, 2324],
+                                        stdin_open=True, tty=True,
+                                        host_config=client.create_host_config(port_bindings= { 2323: 2324}),
+                                        command='/bin/sh',
+                                        detach=True )
+
+    client.start(container_id)
+
+def moveFiles():
+    os.system("docker cp /home/wild_project/py/ha/cr0n/meh/dba-cron cronak:/etc/cron.d/")
+    os.system("docker exec cronak chmod 0644 /etc/cron.d/dba-cron")
+    os.system("docker cp /home/wild_project/py/ha/cr0n/meh/ftpy.py cronak:/home")
+    os.system("docker cp /home/wild_project/py/ha/cr0n/meh/insert_some.py cronak:/home")
+    
+
+def main():
+   ftp_script()
+   cron_script()
+   BuildImage()
+   CreateContainerCron()
+   dba_script()
+   moveFiles()
+
+
+if __name__ == "__main__":
+    main()
