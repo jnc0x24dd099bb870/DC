0ecea3a28c8c954cc8668a4c8bfde62633184294
jnc0x24dd099bb870 <35341430+jnc0x24dd099bb870@users.noreply.github.com>
2019-11-01 02:27:12 +0200

    Update test_folder.py

--- pipel1line/process/test_folder.py
+++ pipel1line/process/test_folder.py
@@ -2,14 +2,13 @@
 
 import os
 
-
 class Folder:
-    folder="/home/tron/wildtest/woohoo" # do not to change this accordingly
-f1=Folder()
 
-def create_folder():
-    if not os.path.exists(f1.folder):
+    folder="/home/tron/wildtest/woohoo"
+    if not os.path.exists(folder):
         print("folder does not exist... creating...")
-        os.makedirs(f1)
+        os.makedirs(folder)
     else:
         print("folder exists...")
+
+f1=Folder()
