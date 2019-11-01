cbbfdc6ff7e772b5fa39759e87e540390bcbaa05
jnc0x24dd099bb870 <35341430+jnc0x24dd099bb870@users.noreply.github.com>
2019-11-01 02:36:07 +0200

    Update script_cron.py

--- pipel1line/process/script_cron.py
+++ pipel1line/process/script_cron.py
@@ -2,9 +2,11 @@
 
 import os
 
+from test_folder import Folder
+
 def cron_script():
     str_cron="""* * * * * root  /usr/bin/python3  /home/insert_some.py"""
 
-    g_g = open("/home/wild_project/py/ha/cr0n/meh/dba-cron", "w")
+     g_g = open(Folder.folder+"/dba-cron", "w")
     g_g.write(str_cron)
     g_g.close()
